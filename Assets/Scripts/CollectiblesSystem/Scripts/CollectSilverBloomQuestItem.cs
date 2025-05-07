887e9dceaf9f581353e5735fef30b08e93be8a78
M-dev-acc <75662164+M-dev-acc@users.noreply.github.com>
2025-05-07 06:45:41 +0530

    Initialise: first quest

---
+++ Assets/Scripts/CollectiblesSystem/Scripts/CollectSilverBloomQuestItem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CollectSilverBloomQuestItem : MonoBehaviour
+{
+    public CollectibleItemSO silverBloomQuestItem;
+    private void OnTriggerEnter(Collider other)
+    {
+        silverBloomQuestItem.ApplyEffect(other.gameObject);
+    }
+}
