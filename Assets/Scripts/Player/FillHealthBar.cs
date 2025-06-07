1123f5bf14f7872b9d8108051197aa25ed6e3130
bharat mohite <bharatmohite2201@gmail.com>
2025-06-07 07:18:47 +0530

    HUD Updated
    HUD Updated.Cactus and flowers replaced.

--- Assets/Scripts/Player/FillHealthBar.cs
+++ Assets/Scripts/Player/FillHealthBar.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 
 public class FillHealthBar : MonoBehaviour
 {
+     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
     public CharacterMovement player;
     // public Image fillImage;
-    private Slider slider;
+
     void Awake()
     {
+
         slider = GetComponent<Slider>();
+        fill.color = gradient.Evaluate(1f);
     }
     void Update()
     {
         slider.value = HealthManager.Instance.GetStaminaPercent();
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    
     }
 }
